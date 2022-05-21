d35df6b0fc4b91a7a8dc47bbd30b8153c7ed6710
Elpis-alpha <elpis409@gmail.com>
2022-05-21 11:42:44 +0100

    redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
+
 import App from './App';
+
+import thunk from 'redux-thunk';
+
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import rootReducer from './store/reducers/index';
+
+import { Provider } from 'react-redux';
+
+// Redux Setup
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
+
   <React.StrictMode>
-    <App />
+
+    <Provider store={store}>
+
+      <App />
+
+    </Provider>
+
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
